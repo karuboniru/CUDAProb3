27129946988542999f5e7b05a96c1172d73bc906
Qiyu Yan <yanqiyu17@mails.ucas.ac.cn>
2024-12-07 16:54:01 +0800

    Do conjugate of U matrix for anti-neutrino

--- cpupropagator.hpp
+++ cpupropagator.hpp
@@ -87,7 +87,17 @@ namespace cudaprob3{
                 throw std::runtime_error("CpuPropagator::calculateProbabilities. production height was not set");
 
             // set neutrino parameters for core physics functions
-            physics::setMixMatrix_host(this->Mix_U.data());
+            switch (type) {
+            case NeutrinoType::Neutrino:
+                physics::setMixMatrix_host(this->Mix_U.data());
+                break;
+            case NeutrinoType::Antineutrino:
+                auto Mix_U_conj = this->Mix_U;
+                for (auto &m : Mix_U_conj) {
+                    m.im = -m.im;
+                }
+                physics::setMixMatrix_host(Mix_U_conj.data());
+            }
             physics::setMassDifferences_host(this->dm.data());
 
             physics::calculate(type, this->cosineList.data(), this->cosineList.size(),
