f85e32611c6fe1970c9464fa9ddf0caeec081e58
Q YAN <yanqiyu01@gmail.com>
2026-04-26 21:07:31 +0800

    rewrite: promote new_project to project root, remove old flat-file structure

--- documentation/html/search/classes_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['cpupropagator',['CpuPropagator',['../classcudaprob3_1_1CpuPropagator.html',1,'cudaprob3']]],
-  ['cudapropagator',['CudaPropagator',['../classcudaprob3_1_1CudaPropagator.html',1,'cudaprob3']]],
-  ['cudapropagatorsingle',['CudaPropagatorSingle',['../classcudaprob3_1_1CudaPropagatorSingle.html',1,'cudaprob3']]]
-];
--- documentation/html/search/classes_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['propagator',['Propagator',['../classcudaprob3_1_1Propagator.html',1,'cudaprob3']]]
-];
